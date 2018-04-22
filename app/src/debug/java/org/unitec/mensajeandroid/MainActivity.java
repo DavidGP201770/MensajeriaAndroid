08eb381f9e15fa6c18e788dafe520e79251eab3b
DavidGP201770 <davicho201170@gmail.com>
2018-04-22 17:36:11 -0500

    hjeje

--- app/src/debug/java/org/unitec/mensajeandroid/MainActivity.java
+++ app/src/debug/java/org/unitec/mensajeandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package org.unitec.mensajeandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //AQUI GENERAMOS LA NAVEGACHION :V
+        //NAVEGACION DE REGISTRO
+
+        findViewById(R.id.registrarse).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),RegistroActivity.class);
+                startActivity(i);
+            }
+        });
+
+        findViewById(R.id.enviar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MensajeActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
