75f6787d51f9bc0251ee25db067d70fcb36ec779
DavidGP201770 <davicho201170@gmail.com>
2018-04-22 20:36:27 -0500

    Implementando registro

--- app/src/main/java/org/unitec/mensajeandroid/RegistroActivity.java
+++ app/src/main/java/org/unitec/mensajeandroid/RegistroActivity.java
@@ -1,7 +1,16 @@
 package org.unitec.mensajeandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -9,5 +18,67 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+
+
+
+        //BOTON REGISTRAR
+        Button botonregistrar;
+
+        botonregistrar = (Button) findViewById(R.id.darregistro);
+        botonregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TAREA DE REGISTRAR
+                TareaRegistrar tareaRegistrar = new TareaRegistrar();
+                tareaRegistrar.execute(null,null,null);
+
+
+
+
+            }
+        });
+
+        //FIN BOTON REGISTRAR
+
     }
+
+    public class TareaRegistrar extends AsyncTask<String, String, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            super.onPostExecute(s);
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+            restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+            Usuario usu=new Usuario();
+
+            usu.getNickname();
+
+            String resultado = restTemplate.postForObject("https://jc-moguito.herokuapp.com/api/mensajito", mensajito, String.class);
+
+            ObjectMapper mapper=new ObjectMapper();
+            try {
+                Estatus estatus=mapper.readValue(resultado, Estatus.class);
+                System.out.println("ESTATUS DEL SERVICIO: "+estatus.isSuccess());
+            } catch (Exception e) {
+                System.out.println("ERROR "+e.getMessage());
+            }
+
+            return null;
+        }
+    }
+
 }
